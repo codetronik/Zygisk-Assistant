0b95387176c846aed8701dfff0d0e077711dda34
codetronik <codetronik@gmail.com>
2026-03-13 17:30:09 +0900

    Simplify logging by removing NDEBUG condition
    Removed conditional logging macros for debug builds.

--- module/jni/include/logging.hpp
+++ module/jni/include/logging.hpp
@@ -4,13 +4,7 @@
 #include <cerrno>
 #include <cinttypes>
 
-#ifndef NDEBUG
 static constexpr auto TAG = "ZygiskAssistant/JNI";
 #define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, TAG, __VA_ARGS__)
 #define LOGW(...) __android_log_print(ANDROID_LOG_WARN, TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
-#else
-#define LOGD(...)
-#define LOGW(...)
-#define LOGE(...)
-#endif
